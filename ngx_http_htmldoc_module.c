81178ed7b05dc35c829e64816e275156f143c54b
RekGRpth <rekgrpth@gmail.com>
2020-05-02 14:42:03 +0500

    up

--- ngx_http_htmldoc_module.c
+++ ngx_http_htmldoc_module.c
@@ -139,6 +139,10 @@ static ngx_int_t ngx_http_htmldoc_header_filter(ngx_http_request_t *r) {
         case OUTPUT_TYPE_PDF: PSLevel = 0; ngx_str_set(&r->headers_out.content_type, "application/pdf"); break;
         case OUTPUT_TYPE_PS: PSLevel = 3; ngx_str_set(&r->headers_out.content_type, "application/ps"); break;
     }
+    if (!r->headers_out.content_type.data) {
+        ngx_http_core_loc_conf_t *core = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
+        r->headers_out.content_type = core->default_type;
+    }
     r->headers_out.content_type_len = r->headers_out.content_type.len;
     return ngx_http_next_header_filter(r);
 }
